896b213997a3a6d7259a9fe6400feb24e7da584b
Ruslan <ruslan.bur@gmail.com>
2017-03-24 18:34:30 +0200

    week 2 task 4 added  test

---
+++ src/test/java/week2/Task4Test.java
@@ -0,0 +1,34 @@
+package week2;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ * Created by ruslan on 24.03.17.
+ */
+public class Task4Test {
+    Task4 task4 = new Task4();
+
+    @Test(dataProvider = "sortedData", timeOut = 500)
+    public void binarySearchTest(int[] sortedArr, int findNumber, int expectedIndex ) {
+
+        int actualIndex = task4.findIndex(sortedArr);
+        Assert.assertEquals(actualIndex, expectedIndex);
+
+    }
+
+    @DataProvider
+    public Object[][] sortedData() {
+        return new Object[][]{
+            {new int[]{3, 6, 7, 10, 34, 56, 60}, 3, 0},
+            {new int[]{3, 6, 7, 10, 34, 56, 60}, 6, 1},
+            {new int[]{3, 6, 7, 10, 34, 56, 60}, 7, 2},
+            {new int[]{3, 6, 7, 10, 34, 56, 60}, 10, 3},
+            {new int[]{3, 6, 7, 10, 34, 56, 60}, 34, 4},
+            {new int[]{3, 6, 7, 10, 34, 56, 60}, 56, 5},
+            {new int[]{3, 6, 7, 10, 34, 56, 60}, 60, 6},
+            {new int[]{3, 6, 7, 10, 34, 56, 60}, 13, -1},
+        };
+    }
+}
